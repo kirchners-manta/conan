1a7c7182f625d1d4780325f559dc79aeb2dfbe99
Sarah <sarah.volkamer@gmail.com>
2024-05-27 13:51:09 +0200

    [ADD] Add test to verify neighbors within a given distance using KDTree via the "get_neighbors_within_distance" function

--- tests/test_graphene.py
+++ tests/test_graphene.py
@@ -1,5 +1,6 @@
 import random
 
+import numpy as np
 import pytest
 
 from conan.playground.doping_experiment import GrapheneGraph
@@ -21,9 +22,9 @@ class TestGraphene:
         graphene = GrapheneGraph(bond_distance=1.42, sheet_size=(20, 20))
         return graphene
 
-    def test_get_direct_neighbors(self, graphene: GrapheneGraph):
+    def test_get_direct_neighbors_via_bonds(self, graphene: GrapheneGraph):
         """
-        Test to verify the direct neighbors of an atom while considering periodic boundary conditions.
+        Test to verify the direct connected neighbors of an atom while considering periodic boundary conditions.
 
         Parameters
         ----------
@@ -32,18 +33,19 @@ class TestGraphene:
 
         Asserts
         -------
-        Checks if the direct neighbors of the atom with ID 0 match the expected neighbors.
+        Checks if the direct connected neighbors of the atom with ID 0 match the expected neighbors.
         """
         graphene = graphene
-        direct_neighbors = graphene.get_neighbors(atom_id=0, depth=1)
-        expected_neighbors = graphene.get_neighbors(0)
+        direct_neighbors = graphene.get_neighbors_via_edges(atom_id=0, depth=1)
+        expected_neighbors = graphene.get_neighbors_via_edges(0)
         assert set(direct_neighbors) == set(
             expected_neighbors
         ), f"Expected {expected_neighbors}, but got {direct_neighbors}"
 
-    def test_get_neighbors_at_exact_depth(self, graphene: GrapheneGraph):
+    def test_get_neighbors_at_exact_depth_via_bonds(self, graphene: GrapheneGraph):
         """
-        Test to verify the neighbors of an atom at an exact depth while considering periodic boundary conditions.
+        Test to verify the connected neighbors of an atom at an exact depth while considering periodic boundary
+        conditions.
 
         Parameters
         ----------
@@ -52,18 +54,19 @@ class TestGraphene:
 
         Asserts
         -------
-        Checks if the neighbors of the atom with ID 0 at depth 2 match the expected neighbors.
+        Checks if the connected neighbors of the atom with ID 0 at depth 2 match the expected neighbors.
         """
         graphene = graphene
-        depth_neighbors = graphene.get_neighbors(atom_id=0, depth=2, inclusive=False)
+        depth_neighbors = graphene.get_neighbors_via_edges(atom_id=0, depth=2, inclusive=False)
         expected_neighbors = [2, 16, 14, 126, 112, 114]
         assert set(depth_neighbors) == set(
             expected_neighbors
         ), f"Expected {expected_neighbors}, but got {depth_neighbors}"
 
-    def test_get_neighbors_up_to_depth(self, graphene: GrapheneGraph):
+    def test_get_neighbors_up_to_depth_via_bonds(self, graphene: GrapheneGraph):
         """
-        Test to verify the neighbors of an atom up to a certain depth while considering periodic boundary conditions.
+        Test to verify the connected neighbors of an atom up to a certain depth while considering periodic boundary
+        conditions.
 
         Parameters
         ----------
@@ -72,11 +75,36 @@ class TestGraphene:
 
         Asserts
         -------
-        Checks if the neighbors of the atom with ID 0 up to depth 2 (inclusive) match the expected neighbors.
+        Checks if the connected neighbors of the atom with ID 0 up to depth 2 (inclusive) match the expected neighbors.
         """
         graphene = graphene
-        inclusive_neighbors = graphene.get_neighbors(atom_id=0, depth=2, inclusive=True)
+        inclusive_neighbors = graphene.get_neighbors_via_edges(atom_id=0, depth=2, inclusive=True)
         expected_neighbors = [1, 15, 113, 2, 16, 14, 126, 112, 114]
         assert set(inclusive_neighbors) == set(
             expected_neighbors
         ), f"Expected {expected_neighbors}, but got {inclusive_neighbors}"
+
+    def test_get_neighbors_within_distance(self, graphene: GrapheneGraph):
+        """
+        Test to verify neighbors within a given distance using KDTree.
+
+        Parameters
+        ----------
+        graphene : GrapheneGraph
+            The graphene fixture providing the initialized GrapheneGraph instance.
+
+        Asserts
+        -------
+        Checks if the neighbors within a specified distance are correct by calculating the Euclidean distance.
+        """
+        atom_id = 56
+        max_distance = 5
+        neighbors = graphene.get_neighbors_within_distance(atom_id, max_distance)
+
+        atom_position = np.array(graphene.graph.nodes[atom_id]["position"])
+        for neighbor in neighbors:
+            neighbor_position = np.array(graphene.graph.nodes[neighbor]["position"])
+            distance = np.linalg.norm(atom_position - neighbor_position)
+            assert distance <= max_distance, (
+                f"Neighbor {neighbor} at distance {distance} exceeds max_distance " f"{max_distance}"
+            )
