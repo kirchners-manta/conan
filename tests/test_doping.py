efdda2298d7e6dc13e68f4f15ef01ab35d4dea88
Sarah <sarah.volkamer@gmail.com>
2024-10-27 19:43:30 +0100

    [ADD] Test that total percentage needs to be a positive float or integer

--- tests/test_doping.py
+++ tests/test_doping.py
@@ -56,55 +56,20 @@ def optimized_structure():
 
 class TestDopingValidations:
 
-    # def test_adjust_atom_positions(self, optimized_reference_structure):
-    #     """
-    #     Test that the adjusted atom positions closely match the optimized reference structure.
-    #
-    #     Parameters
-    #     ----------
-    #     optimized_reference_structure : tuple
-    #         A tuple containing the optimized positions and element symbols obtained from force field optimization.
-    #
-    #     Raises
-    #     ------
-    #     AssertionError
-    #         If the positions or elements do not match within acceptable tolerances.
-    #     """
-    #     # Unpack the optimized reference structure
-    #     optimized_reference_positions, optimized_reference_elements = optimized_reference_structure
-    #
-    #     # Set the random seed for reproducibility
-    #     random.seed(0)
-    #
-    #     # Create the graphene sheet with the same parameters as the optimized reference structure
-    #     graphene_sheet = GrapheneSheet(bond_distance=1.42, sheet_size=(20, 20))
-    #
-    #     # Apply nitrogen doping with 10% total percentage
-    #     graphene_sheet.add_nitrogen_doping(total_percentage=10, adjust_positions=True)
-    #
-    #     # Extract positions and elements from the optimized structure
-    #     optimized_positions = np.array(
-    #         [graphene_sheet.graph.nodes[node]["position"].to_tuple() for node in sorted(graphene_sheet.graph.nodes())]
-    #     )
-    #     optimized_elements = [
-    #         graphene_sheet.graph.nodes[node]["element"] for node in sorted(graphene_sheet.graph.nodes())
-    #     ]
-    #
-    #     # Ensure that the number of atoms matches
-    #     assert len(optimized_positions) == len(optimized_reference_positions), "Number of atoms does not match."
-    #
-    #     # Compare elements
-    #     assert optimized_elements == optimized_reference_elements, "Element symbols do not match."
-    #
-    #     # Compare positions with tolerances
-    #     npt.assert_allclose(optimized_positions, optimized_reference_positions, atol=1e-5, rtol=1e-5)
-
     @pytest.mark.parametrize("invalid_total_percentage", ["invalid", [], {}])
     def test_total_percentage_type_error(self, graphene_sheet, invalid_total_percentage):
         """
         Test that a ValueError is raised when total_percentage is not a float or int.
         """
         with pytest.raises(ValueError, match="total_percentage must be an int or float"):
+            graphene_sheet.add_nitrogen_doping(total_percentage=invalid_total_percentage)
+
+    @pytest.mark.parametrize("invalid_total_percentage", [-10, 0])
+    def test_invalid_total_percentage(self, graphene_sheet, invalid_total_percentage):
+        """
+        Test that a ValueError is raised when total_percentage is non-positive.
+        """
+        with pytest.raises(ValueError, match="total_percentage must be positive."):
             graphene_sheet.add_nitrogen_doping(total_percentage=invalid_total_percentage)
 
     @pytest.mark.parametrize(
