5876762e6f8e203676c10bfd3dba46428a0e29c1
Leonard Dick <dick@thch.uni-bonn.de>
2025-01-17 02:14:46 +0100

    version is working

--- src/conan/analysis_modules/cnt_fill.py
+++ src/conan/analysis_modules/cnt_fill.py
@@ -7,13 +7,17 @@ import conan.defdict as ddict
 
 
 # check if a given point is within a cylinder
-def points_in_cylinder(pt1, pt2, r, q):
+def points_in_cylinder(pt1, pt2, r, atom_to_check):
+    pt1 = np.array(pt1, dtype=np.float64)
+    pt2 = np.array(pt2, dtype=np.float64)
+    atom_to_check = np.array(atom_to_check, dtype=np.float64)
+
     vec = pt2 - pt1
     const = r * np.linalg.norm(vec)
-    return np.where(
-        (np.dot(q - pt1, vec) >= 0)
-        & (np.dot(q - pt2, vec) <= 0)
-        & (np.linalg.norm(np.cross(q - pt1, vec), axis=1) <= const)
+    return (
+        (np.dot(atom_to_check - pt1, vec) >= 0)
+        & (np.dot(atom_to_check - pt2, vec) <= 0)
+        & (np.linalg.norm(np.cross(atom_to_check - pt1, vec)) <= const)
     )
 
 
@@ -21,6 +25,7 @@ def cnt_loading_mass(traj_file, molecules, an):
     clm = CNTload(traj_file, molecules, an)
     clm.cnt_loading_mass_prep()
     traj_an.process_trajectory(traj_file, molecules, an, clm)
+    clm.cnt_loading_mass_processing()
 
 
 class CNTload:
@@ -33,7 +38,9 @@ class CNTload:
         self.molecules = molecules
         self.an = an
         self.liquid_mass = 0
-        self.total_mass = 0
+        self.proc_frame_counter = 0
+        self.shortening_q = "n"
+        self.shortening = 0
 
     def cnt_loading_mass_prep(self):
         """
@@ -112,9 +119,71 @@ class CNTload:
         self.ring2 = np.array([atom for atom in pore_atoms[pore_atoms["ring"] == 2].index])
 
         ddict.printLog(
-            "Please use global, unwrapped coordinates for this analysis.",
+            "\nPlease use global, unwrapped coordinates for this analysis.",
             " Otherwise the results are potentially erroneous.",
+            color="red",
         )
+
+        self.traj_file.frame0["x"] = self.traj_file.frame0["x"].astype(float)
+        self.traj_file.frame0["y"] = self.traj_file.frame0["y"].astype(float)
+        self.traj_file.frame0["z"] = self.traj_file.frame0["z"].astype(float)
+
+        # calculate the center of geometry for the ring1 atoms
+        ring1 = self.traj_file.frame0.loc[self.ring1]
+        ring1_ref = ring1.iloc[0][["x", "y", "z"]].values
+        ring1_x = ring1["x"].mean()
+        ring1_y = ring1["y"].mean()
+        ring1_z = ring1["z"].mean()
+
+        ring2 = self.traj_file.frame0.loc[self.ring2]
+        ring2_x = ring2["x"].mean()
+        ring2_y = ring2["y"].mean()
+        ring2_z = ring2["z"].mean()
+
+        ring1_array = np.array([ring1_x, ring1_y, ring1_z])
+        ring2_array = np.array([ring2_x, ring2_y, ring2_z])
+
+        self.dist_ring = np.linalg.norm(ring1_array - ring1_ref).round(3)
+
+        self.dist = np.linalg.norm(
+            traj_info.minimum_image_distance(ring1_array, ring2_array, self.traj_file.box_size)
+        ).round(3)
+
+        print("Distance between the rings:", self.dist)
+
+        """The user shall have the option to select an region within the CNTs to calculate the loading mass.
+        This is preferred, as the liquid might behave somewhat differently at the openeing of the CNTs.
+        In Order to do this, the user defines a distane,
+        which is used to diplace the center of geometry of the rings along the CNT axis.
+        The distance is defined in angstroms."""
+
+        # first we need to define the normal vector of the CNT axis. It is the vector between the two rings.
+        # The vector is normalized.
+        cnt_axis = (ring2_array - ring1_array) / np.linalg.norm(ring2_array - ring1_array)
+        print("CNT axis:", cnt_axis)
+
+        self.shortening_q = ddict.get_input(
+            "Do you want to shorten the CNT axis (space to analyze within the CNT)? (y/n): ",
+            self.traj_file.args,
+            "string",
+        )
+        if self.shortening_q == "y":
+            self.shortening = float(
+                ddict.get_input(
+                    "Please enter the amount you want the axis to be shortened",
+                    " (amount is subtracted from both sides): ",
+                    self.traj_file.args,
+                    "float",
+                )
+            )
+
+            ring1_array = ring1_array + self.shortening * cnt_axis
+            ring2_array = ring2_array - self.shortening * cnt_axis
+            self.dist = np.linalg.norm(
+                traj_info.minimum_image_distance(ring1_array, ring2_array, self.traj_file.box_size)
+            ).round(3)
+
+        print("Distance between the rings:", self.dist)
 
     def analyze_frame(self, split_frame, frame_counter):
         """
@@ -123,7 +192,7 @@ class CNTload:
         for each open end of the CNT. (ring1, ring2)
 
         """
-        print(split_frame)
+        # print(split_frame)
         # turn the X, Y and Z values into floats
         split_frame["X"] = split_frame["X"].astype(float)
         split_frame["Y"] = split_frame["Y"].astype(float)
@@ -137,7 +206,6 @@ class CNTload:
         ring1_z = ring1["Z"].mean()
 
         ring2 = split_frame.loc[self.ring2]
-        ring2_ref = ring2.iloc[0][["X", "Y", "Z"]].values
         ring2_x = ring2["X"].mean()
         ring2_y = ring2["Y"].mean()
         ring2_z = ring2["Z"].mean()
@@ -145,17 +213,58 @@ class CNTload:
         ring1_array = np.array([ring1_x, ring1_y, ring1_z])
         ring2_array = np.array([ring2_x, ring2_y, ring2_z])
 
-        print(ring1_array, ring2_array)
-
         # calculate the distance between the mean posistion and a reference atom of the same ring
-        dist_ring1 = np.linalg.norm(traj_info.minimum_image_distance(ring1_array, ring1_ref, self.traj_file.box_size))
-        dist_ring2 = np.linalg.norm(traj_info.minimum_image_distance(ring2_array, ring2_ref, self.traj_file.box_size))
+        self.dist_ring = np.linalg.norm(ring1_array - ring1_ref).round(3)
 
-        print(dist_ring1, dist_ring2)
+        "This is the shortening part regarding the cnt space to observe" ""
+        # calculate the normalized vector between the two rings
+        cnt_axis = (ring2_array - ring1_array) / np.linalg.norm(ring2_array - ring1_array)
+        # print("CNT axis:", cnt_axis)
 
-        dist = np.linalg.norm(traj_info.minimum_image_distance(ring1_array, ring2_array, self.traj_file.box_size))
+        ring1_array = ring1_array + self.shortening * cnt_axis
+        ring2_array = ring2_array - self.shortening * cnt_axis
+        self.dist = np.linalg.norm(
+            traj_info.minimum_image_distance(ring1_array, ring2_array, self.traj_file.box_size)
+        ).round(3)
 
-        print(dist)
+        # print("Ring1 array:", ring1_array)
+        # print("Ring2 array:", ring2_array)
+        # print("Distance between the rings:", self.dist)
 
         """ Now we calculate the mass of the liquid within the CNTs.
-        First identify which species are within the CNTs"""
+        First identify which species are within the CNTs.
+        For this use the points in cylider function."""
+
+        liquid_atoms = split_frame[split_frame["Struc"].str.contains("Liquid")].copy()
+
+        # get the atom positions of the current frame
+        atom_positions = np.array(
+            [[atom["X"], atom["Y"], atom["Z"], atom["Mass"], atom.index] for index, atom in liquid_atoms.iterrows()],
+            dtype=object,
+        )
+
+        # check if a liquid atom is within the CNT
+        for atom_position in atom_positions:
+            if points_in_cylinder(ring1_array, ring2_array, self.dist_ring, atom_position[:3]) is True:
+                # print(atom_position[3])
+                # add the mass of the given atom to the total mass of the liquid within the CNT
+                self.liquid_mass += atom_position[3]
+                # print(self.liquid_mass)
+
+        print("\nTotal liquid mass:", self.liquid_mass.round(3))
+
+        self.proc_frame_counter += 1
+
+    def cnt_loading_mass_processing(self):
+        """
+        Calculate the loading mass of the liquid within the CNTs.
+        """
+        print(self.liquid_mass)
+        # print the number of processed frames
+        print(f"Processed frames: {self.proc_frame_counter}")
+        self.liq_mass_per_frame = self.liquid_mass / self.proc_frame_counter
+        print(f"Average loading mass per frame: {self.liq_mass_per_frame}")
+        # print the loading mass of the liquid per angstrom
+        self.liq_mass_per_angstrom = self.liq_mass_per_frame / self.dist
+        print(f"Loading mass per angstrom: {self.liq_mass_per_angstrom}")
+        self.liquid_mass = 0
