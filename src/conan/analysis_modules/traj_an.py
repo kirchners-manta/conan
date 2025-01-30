e710707f60e06d6be5237d48061d1acc03e91b30
Leonard Dick <dick@thch.uni-bonn.de>
2025-01-30 23:43:10 +0100

    add analysis option 11

--- src/conan/analysis_modules/traj_an.py
+++ src/conan/analysis_modules/traj_an.py
@@ -88,6 +88,7 @@ class Analysis:
         ddict.printLog("(8) Calculate the density along the axes.")
         ddict.printLog("(9) Calculate the velocity along the axes.")
         ddict.printLog("(10) Calculate the (root) mean square displacement of the liquid in the CNT.")
+        ddict.printLog("(11) Calculate the mass of the liquid in the CNT.")
 
         analysis_choice2 = int(ddict.get_input("What analysis should be performed?:  ", traj_file.args, "int"))
         analysis_choice2_options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
