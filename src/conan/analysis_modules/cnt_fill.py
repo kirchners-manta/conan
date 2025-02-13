3de033683210944c81b435efa57eb9a262b54315
LeonardDick <dick.leonard96@gmail.com>
2025-02-13 15:38:06 -0800

    [MOD] tracking changes in radii and lenght of CNT over time

--- src/conan/analysis_modules/cnt_fill.py
+++ src/conan/analysis_modules/cnt_fill.py
@@ -30,6 +30,28 @@ def cnt_loading_mass(traj_file, molecules, an):
     clm.cnt_loading_mass_processing()
 
 
+def plot_and_save_results(masses_df, avg_mass, name):
+    """
+    Plot the results of the confined mass calculation
+    """
+    fig, ax = plt.subplots()
+
+    ax.plot(masses_df.iloc[1:], label="Frame", color="#440154")
+    ax.plot(masses_df["5_frame_average"], label="5 frame average", color="#21908C")
+    ax.plot(masses_df["10_frame_average"], label="10 frame average", color="#5ec962")
+    ax.plot(masses_df["50_frame_average"], label="50 frame average", color="#fde725")
+    ax.axhline(y=avg_mass, color="darkgray", linestyle="--", label="Average mass")
+
+    ax.set_xlabel("Frame")
+    ax.set_ylabel("Mass / u")
+    ax.legend()
+    ax.grid()
+
+    # save the plot
+    fig.savefig(f"{name}.png", dpi=300)
+    masses_df.to_csv(f"{name}.csv")
+
+
 class CNTload:
     """
     Class to calculate the loading mass of the liquid within the carbon nanotube.
@@ -48,12 +70,7 @@ class CNTload:
         initializes the variables, the number of CNTs and identifies the most outwards atoms of the CNTs.
         """
 
-        # get number of CNTs in the trajectory file. (for now we consider all pores to be CNTs, as the CNT definition
-        # describes just frozen CNT along the z-axis).
-        # extract all entries from the frame0 dataframe where the Struc column contains "Pore" and store it
-        # in a new dataframe called pore_atoms.
         pore_atoms = self.traj_file.frame0[self.traj_file.frame0["Struc"].str.contains("Pore")].copy()
-        # drop the "Pore" string from the Struc column and just keep the number
         pore_atoms.loc[:, "Struc"] = pore_atoms["Struc"].str.replace("Pore", "")
         pore_atoms.loc[:, "Struc"] = pore_atoms["Struc"].astype(int)
         pore_No = pore_atoms["Struc"].nunique()
@@ -107,8 +124,6 @@ class CNTload:
             for j in molecules[i]:
                 pore_atoms.loc[j, "ring"] = i + 1
 
-        # ddict.printLog(pore_atoms[pore_atoms["bonds"] == 2])
-
         # save the index of ring1 and ring2 atoms to seperate numpy arrays
         self.ring1 = np.array([atom for atom in pore_atoms[pore_atoms["ring"] == 1].index])
         self.ring2 = np.array([atom for atom in pore_atoms[pore_atoms["ring"] == 2].index])
@@ -141,24 +156,21 @@ class CNTload:
         ring2_array = np.array([ring2_x, ring2_y, ring2_z])
         ring1_ref = np.array([ring1_ref[0], ring1_ref[1], ring1_ref[2]])
 
-        # self.dist_ring = np.linalg.norm(traj_info.minimum_image_distance(
-        # ring1_array, ring1_ref, self.traj_file.box_size))
         self.dist_ring = np.linalg.norm(ring1_array - ring1_ref)
-        ddict.printLog(f"Radius of the CNT: {np.round(self.dist_ring, 3)}")
 
-        # self.dist = np.linalg.norm(traj_info.minimum_image_distance(
-        # ring1_array, ring2_array, self.traj_file.box_size))
         self.dist = np.linalg.norm(ring1_array - ring2_array)
+        ddict.printLog(f"Radius of the CNT: {np.round(self.dist_ring, 3)}")
+        ddict.printLog(f"Length of the CNT: {np.round(self.dist, 3)}")
 
-        ddict.printLog(f"Distance between the rings: {np.round(self.dist, 3)}")
-
-        """The user shall have the option to select an region within the CNTs to calculate the loading mass.
+        """
+        The user shall have the option to select an region within the CNTs to calculate the loading mass.
         This is preferred, as the liquid might behave somewhat differently at the openeing of the CNTs.
         In Order to do this, the user defines a distane,
         which is used to diplace the center of geometry of the rings along the CNT axis.
-        The distance is defined in angstroms."""
+        The distance is defined in angstroms.
+        """
 
-        # first we need to define the normal vector of the CNT axis. It is the vector between the two rings.
+        # Define the normal vector of the CNT axis. It is the vector between the two rings.
         # The vector is normalized.
         cnt_axis = (ring2_array - ring1_array) / np.linalg.norm(ring2_array - ring1_array)
         ddict.printLog(f"CNT axis: {cnt_axis}")
@@ -171,8 +183,7 @@ class CNTload:
         if self.shortening_q == "y":
             self.shortening = float(
                 ddict.get_input(
-                    "Please enter the amount you want the axis to be shortened"
-                    " (amount is subtracted from both sides): ",
+                    "Please enter the amount you want the axis to be shortened" " (from both sides): ",
                     self.traj_file.args,
                     "float",
                 )
@@ -180,32 +191,32 @@ class CNTload:
 
             ring1_array = ring1_array + self.shortening * cnt_axis
             ring2_array = ring2_array - self.shortening * cnt_axis
-            # self.dist = np.linalg.norm(traj_info.minimum_image_distance(
-            # ring1_array, ring2_array, self.traj_file.box_size))
             self.dist = np.linalg.norm(ring1_array - ring2_array)
         print("Length of CNT axis considered:", self.dist.round(3))
 
-        # finally set up an array to store the liquid mass each time frame which is processed.
+        # array to store the liquid mass each time frame which is processed.
         self.liquid_mass = 0
         self.frame_masses = np.array([])
+
+        # array to store the ring-ring distances and the radii of the CNTs
+        self.ring_ring_distances = np.array([])
+        self.ring_radii = np.array([])
 
     def analyze_frame(self, split_frame, frame_counter):
         """
         Calculate the loading mass of the liquid within the CNTs.
         For this we first need to calculate the center of geometry
         for each open end of the CNT. (ring1, ring2)
-
         """
 
         frame_mass = 0
-        # print(split_frame)
-        # turn the X, Y and Z values into floats
         split_frame["X"] = split_frame["X"].astype(float)
         split_frame["Y"] = split_frame["Y"].astype(float)
         split_frame["Z"] = split_frame["Z"].astype(float)
 
-        # calculate the center of geometry for the ring1 atoms
+        # calculate the center of geometry of the rings
         ring1 = split_frame.loc[self.ring1]
+        ring1_ref = ring1.iloc[0][["x", "y", "z"]].values
         ring1_x = ring1["X"].mean()
         ring1_y = ring1["Y"].mean()
         ring1_z = ring1["Z"].mean()
@@ -217,22 +228,28 @@ class CNTload:
 
         ring1_array = np.array([ring1_x, ring1_y, ring1_z])
         ring2_array = np.array([ring2_x, ring2_y, ring2_z])
+        ring1_ref = np.array([ring1_ref[0], ring1_ref[1], ring1_ref[2]])
+
+        dist_ring = np.linalg.norm(ring1_array - ring1_ref)
+        self.ring_radii = np.append(dist_ring)
 
         """
         This is the shortening part regarding the cnt space to observe
         """
-        # calculate the normalized vector between the two rings
+        # normalized vector between the two rings
         cnt_axis = (ring2_array - ring1_array) / np.linalg.norm(ring2_array - ring1_array)
 
         ring1_array = ring1_array + self.shortening * cnt_axis
         ring2_array = ring2_array - self.shortening * cnt_axis
-        # self.dist = np.linalg.norm(
-        #    traj_info.minimum_image_distance(ring1_array, ring2_array, self.traj_file.box_size))
         self.dist = np.linalg.norm(ring1_array - ring2_array)
 
-        """ Calculate the mass of the liquid within the CNTs.
+        self.ring_ring_distances = np.append(self.dist)
+
+        """
+        Calculate the mass of the liquid within the CNTs.
         First identify which species are within the CNTs.
-        For this use the points in cylider function."""
+        For this use the points in cylider function.
+        """
 
         liquid_atoms = split_frame[split_frame["Struc"].str.contains("Liquid")].copy()
 
@@ -257,11 +274,11 @@ class CNTload:
         Calculate the loading mass of the liquid within the CNTs.
         """
 
-        self.liq_mass_per_frame = self.liquid_mass / self.proc_frame_counter
-        self.liq_mass_per_angstrom = self.liq_mass_per_frame / self.dist
+        self.avg_mass_per_frame = self.liquid_mass / self.proc_frame_counter
+        self.avg_mass_per_angstrom = self.avg_mass_per_frame / self.dist
 
-        ddict.printLog(f"Average confined mass: {np.round(self.liq_mass_per_frame, 5)}")
-        ddict.printLog(f"Average mass per \u00c5: {np.round(self.liq_mass_per_angstrom, 5)}")
+        ddict.printLog(f"Average confined mass: {np.round(self.avg_mass_per_frame, 5)}")
+        ddict.printLog(f"Average mass per \u00c5: {np.round(self.avg_mass_per_angstrom, 5)}")
         self.liquid_mass = 0
 
         pd_frame_masses = pd.DataFrame(self.frame_masses)
@@ -272,44 +289,24 @@ class CNTload:
             ddict.printLog("CNT is empty, no liquid is present.", color="red")
             return
 
-        # add a new column with the average of 5, 10 and 50 frames
+        # average of 5, 10 and 50 frames
         pd_frame_masses["5_frame_average"] = pd_frame_masses["Frame_masses"].rolling(window=5).mean().shift(-2)
         pd_frame_masses["10_frame_average"] = pd_frame_masses["Frame_masses"].rolling(window=10).mean().shift(-5)
         pd_frame_masses["50_frame_average"] = pd_frame_masses["Frame_masses"].rolling(window=50).mean().shift(-25)
 
-        # plot the results
-        fig, ax = plt.subplots()
-        ax.plot(pd_frame_masses["Frame_masses"], label="Frame", color="#440154")
-        ax.plot(pd_frame_masses["5_frame_average"], label="5 frame average", color="#21908C")
-        ax.plot(pd_frame_masses["10_frame_average"], label="10 frame average", color="#5ec962")
-        ax.plot(pd_frame_masses["50_frame_average"], label="50 frame average", color="#fde725")
-        ax.axhline(y=self.liq_mass_per_frame, color="darkgray", linestyle="--", label="Average mass")
+        pd_mass_per_angstrom = pd_frame_masses.div(self.dist, axis=0).copy()
 
-        ax.set_xlabel("Frame number")
-        ax.set_ylabel("Confined mass / u")
-        ax.legend()
-        ax.grid()
+        plot_and_save_results(pd_frame_masses, self.avg_mass_per_frame, "frame_masses")
+        plot_and_save_results(pd_mass_per_angstrom, self.avg_mass_per_angstrom, "mass_per_angstrom")
 
-        # save the plot
-        fig.savefig("conf_mass_temp.png", dpi=300)
-
-        pd_mass_per_angstrom = pd_frame_masses.copy().div(self.dist)
-        pd_mass_per_angstrom.rename(columns={"Frame_masses": "Mass_per_angstrom"}, inplace=True)
-
-        # plot the results per angstrom
-        fig, ax = plt.subplots()
-        ax.plot(pd_mass_per_angstrom["Mass_per_angstrom"], label="Frame", color="#440154")
-        ax.plot(pd_mass_per_angstrom["5_frame_average"], label="5 frame average", color="#21908C")
-        ax.plot(pd_mass_per_angstrom["10_frame_average"], label="10 frame average", color="#5ec962")
-        ax.plot(pd_mass_per_angstrom["50_frame_average"], label="50 frame average", color="#fde725")
-        ax.axhline(y=self.liq_mass_per_angstrom, color="darkgray", linestyle="--", label="Average mass")
-
-        ax.set_xlabel("Frame Number")
-        ax.set_ylabel("Confined mass / u")
-        ax.legend()
-        ax.grid()
-
-        # save the data
-        fig.savefig("conf_mass_t_ang.png", dpi=300)
         pd_frame_masses.to_csv("frame_masses.csv")
         pd_mass_per_angstrom.to_csv("mass_per_angstrom.csv")
+
+        # save the ring-ring distances and the radii of the CNTs
+        pd_ring_ring_distances = pd.DataFrame(self.ring_ring_distances)
+        pd_ring_ring_distances.columns = ["Ring_ring_distances"]
+        pd_ring_ring_distances.to_csv("ring_ring_distances.csv")
+
+        pd_ring_radii = pd.DataFrame(self.ring_radii)
+        pd_ring_radii.columns = ["Ring_radii"]
+        pd_ring_radii.to_csv("ring_radii.csv")
